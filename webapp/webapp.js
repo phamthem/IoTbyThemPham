c39a38a8472327e42c7a99bb620331286c923915
phamthem <38209756+phamthem@users.noreply.github.com>
2018-05-07 21:22:19 +0700

    Update webapp.js

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -20,7 +20,7 @@ angular.module('myApp', [
 	////Khu 1 -- Khu cài đặt tham số 
     //cài đặt một số tham số test chơi
 	//dùng để đặt các giá trị mặc định
-    $scope.CamBienMua = "Không biết nữa ahihi,  thấy có thằng nào cập nhập hết";
+    $scope.CamBienMua = "không có tín hiệu từ cảm biến";
     $scope.leds_status = [1, 1]
 	$scope.lcd = ["", ""]
 	$scope.servoPosition = 0
