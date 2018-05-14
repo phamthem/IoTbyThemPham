c3134e0d275e4177ee4ce51af1d587708048ae3d
phamthem <38209756+phamthem@users.noreply.github.com>
2018-05-14 12:39:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ var server = http.Server(app)
 
 var io = socketio(server);				//#Phải khởi tạo io sau khi tạo app
 
-var webapp_nsp = io.of('/webapp')				//namespace của webapp
-var esp8266_nsp = io.of('/esp8266')				//namespace của esp8266
+var webapp_nsp = io.of('/webapp')			//namespace của webapp
+var esp8266_nsp = io.of('/esp8266')			//namespace của esp8266
 
-var middleware = require('socketio-wildcard')();		//Để có thể bắt toàn bộ lệnh!
-esp8266_nsp.use(middleware);					//Khi esp8266 emit bất kỳ lệnh gì lên thì sẽ bị bắt
-webapp_nsp.use(middleware);					//Khi webapp emit bất kỳ lệnh gì lên thì sẽ bị bắt
+var middleware = require('socketio-wildcard')();	//Để có thể bắt toàn bộ lệnh!
+esp8266_nsp.use(middleware);				//Khi esp8266 emit bất kỳ lệnh gì lên thì sẽ bị bắt
+webapp_nsp.use(middleware);				//Khi webapp emit bất kỳ lệnh gì lên thì sẽ bị bắt
 
 server.listen(process.env.PORT || PORT);										// Cho socket server (chương trình mạng) lắng nghe ở port 3484
 console.log("Server nodejs cua chung ta dang chay o dia chi ip nay: " + ip.address() + ":" + PORT)
@@ -45,7 +45,7 @@ esp8266_nsp.on('connection', function(socket) {
 	
 	//nhận được bất cứ lệnh nào
 	socket.on("*", function(packet) {
-		console.log("esp8266 rev and send to webapp packet: ", packet.data) //in ra để debug
+		console.log("esp8266 goi len webapp: ", packet.data) //in ra để debug
 		var eventName = packet.data[0]
 		var eventJson = packet.data[1] || {} //nếu gửi thêm json thì lấy json từ lệnh gửi, không thì gửi chuỗi json rỗng, {}
 		webapp_nsp.emit(eventName, eventJson) //gửi toàn bộ lệnh + json đến webapp
@@ -54,7 +54,6 @@ esp8266_nsp.on('connection', function(socket) {
 
 //Bắt các sự kiện khi webapp kết nối
 webapp_nsp.on('connection', function(socket) {
-	
 	console.log('webapp connected')
 	//Khi webapp socket bị mất kết nối
 	socket.on('disconnect', function() {
@@ -62,8 +61,8 @@ webapp_nsp.on('connection', function(socket) {
 		})
 	
 	socket.on('*', function(packet) {
-		console.log("webapp rev and send to esp8266 packet: ", packet.data) //in ra để debug
-		var eventName = packet.data[0]
+		console.log("webapp goi ve 8266 ", packet.data) //in ra để debug
+		var eventName = packet.data[0]		//ten cua lenh goi
 		var eventJson = packet.data[1] || {} 	//nếu gửi thêm json thì lấy json từ lệnh gửi, không thì gửi chuỗi json rỗng, {}
 		esp8266_nsp.emit(eventName, eventJson) 	//gửi toàn bộ lệnh + json đến esp8266
 		});
