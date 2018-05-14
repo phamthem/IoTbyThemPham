a097d7a01776c7232f5ced17d10123c054c39f84
phamthem <38209756+phamthem@users.noreply.github.com>
2018-05-14 21:26:44 +0700

    Update webapp.js

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -48,7 +48,11 @@ angular.module('myApp', [
 		$scope.nhietdo = json.nhiet_do
 		$scope.doam = json.do_am
 		})
-	
+	//Khi nhận được lệnh TRANGTHAIDEN
+	mySocket.on('TRANGTHAIDEN', function(json) {
+		console.log("recv TRANGTHAIDEN", json)
+		$scope.trangthaiden = (json.trangthaiden == 1) ? "đèn đã bật" : "đèn đã tắt"
+		})
 // Khu 4 -- Những dòng code sẽ được thực thi khi kết nối với Arduino (thông qua socket server)
 	mySocket.on('connect', function() {
 		console.log("connected")
