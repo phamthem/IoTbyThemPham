23768a4c106f1b75ba1ee9912911093e68e216d9
phamthem <38209756+phamthem@users.noreply.github.com>
2018-06-09 17:01:36 +0700

    Update webapp.js

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -22,27 +22,48 @@ angular.module('myApp', [
 	//dùng để đặt các giá trị mặc định
     	$scope.nhietdo = "Chưa kết nối";
 	$scope.doam = "Chưa kết nối";
-	$scope.trangthaiden = "Chưa kết nối";
-	$scope.trangthaiphunsuong = "Chưa kết nối";
+	$scope.trangthairelay1 = "Chưa kết nối";
+	$scope.trangthairelay2 = "Chưa kết nối";
+	$scope.trangthairelay3 = "Chưa kết nối";
+	$scope.trangthairelay4 = "Chưa kết nối";
 	
 	
 //khu 2 -- gởi json về cho index.js đển chuyển về cho arduino
 	//Cách gửi tham số 1: dùng biến toàn cục! $scope.<tên biến> là biến toàn cục
-	$scope.batden = function() {
-		console.log("ham bat den duoc thuc thi")
-		mySocket.emit("BATDEN")		//gởi chuỗi BATDEN về arduino
+	$scope.dongrelay1 = function() {
+		console.log("ham dongrelay1 duoc thuc thi")
+		mySocket.emit("DRL1")		//gởi chuỗi về arduino
 		}
-	$scope.tatden = function() {
-		console.log("ham Tat den duoc thuc thi")
-		mySocket.emit("TATDEN")		//gởi chuỗi TATDEN về arduino
+	$scope.ngatrelay1 = function() {
+		console.log("ham ngatrelay1 duoc thuc thi")
+		mySocket.emit("NRL1")		//gởi chuỗi về arduino
 		}
-	$scope.batphunsuong = function() {
-		console.log("ham bat phun suong duoc thuc thi")
-		mySocket.emit("BATPS")		//gởi chuỗi BATDEN về arduino
+	/////////////////////////////////////////////////////////////
+	$scope.dongrelay2 = function() {
+		console.log("ham dongrelay2 duoc thuc thi")
+		mySocket.emit("DRL2")		//gởi chuỗi về arduino
 		}
-	$scope.tatphunsuong = function() {
-		console.log("ham Tat phun suong duoc thuc thi")
-		mySocket.emit("TATPS")		//gởi chuỗi TATDEN về arduino
+	$scope.ngatrelay2 = function() {
+		console.log("ham ngatrelay2 duoc thuc thi")
+		mySocket.emit("NRL2")		//gởi chuỗi về arduino
+		}
+	/////////////////////////////////////////////////////////////
+	$scope.dongrelay3 = function() {
+		console.log("ham dongrelay3 duoc thuc thi")
+		mySocket.emit("DRL3")		//gởi chuỗi về arduino
+		}
+	$scope.ngatrelay3 = function() {
+		console.log("ham ngatrelay3 duoc thuc thi")
+		mySocket.emit("NRL3")		//gởi chuỗi về arduino
+		}
+	//////////////////////////////////////////////////////////////
+	$scope.dongrelay4 = function() {
+		console.log("ham dongrelay4 duoc thuc thi")
+		mySocket.emit("DRL4")		//gởi chuỗi về arduino
+		}
+	$scope.ngatrelay4 = function() {
+		console.log("ham ngatrelay4 duoc thuc thi")
+		mySocket.emit("NRL4")		//gởi chuỗi về arduino
 		}
 //Khu 3 -- Nhận dữ liệu từ Arduno gửi lên (thông qua ESP8266 rồi socket server truyền tải!)
 	//các sự kiện từ Arduino gửi lên (thông qua esp8266, thông qua server)
@@ -56,8 +77,10 @@ angular.module('myApp', [
 	//Khi nhận được lệnh P_HOI
 	mySocket.on('P_HOI', function(json) {
 		console.log("recv TRANGTHAIDEN", json)
-		$scope.trangthaiden = (json.trangthaiden == 1) ? "đèn đã bật" : "đèn đã tắt"
-		$scope.trangthaiphunsuong = (json.trangthaiphunsuong == 1) ? "Máy phun sương đã bật" : "Máy phun sương đã tắt"
+		$scope.trangthairelay1 = (json.tt_relay1 == 1) ? "thiết bị này đã bật" : "thiết bị này đã tắt"
+		$scope.trangthairelay1 = (json.tt_relay2 == 1) ? "thiết bị này đã bật" : "thiết bị này đã tắt"
+		$scope.trangthairelay1 = (json.tt_relay3 == 1) ? "thiết bị này đã bật" : "thiết bị này đã tắt"
+		$scope.trangthairelay1 = (json.tt_relay4 == 1) ? "thiết bị này đã bật" : "thiết bị này đã tắt"
 		})
 	
 // Khu 4 -- Những dòng code sẽ được thực thi khi kết nối với Arduino (thông qua socket server)
