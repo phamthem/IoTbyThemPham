a727d0575625b310ac637fc84450a3bb136cd926
phamthem <38209756+phamthem@users.noreply.github.com>
2018-06-14 23:06:48 +0700

    Update webapp.js

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -125,25 +125,19 @@ angular.module('IoT').controller('Home', function($scope, mySocket) {
 });
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-angular.module('IoT').controller('LoginController', function($scope, $location, $window,page) {
-    page.setPage("Login","login-layout");
-    $scope.user = {};
-    $scope.loginUser=function()
-    {
-        var username=$scope.user.name;
-        var password=$scope.user.password;
-        if(username=="admin" && password=="admin123")
-        {
-            page.setUser($scope.user);
-            $location.path( "/home" );
-        }
-        else
-        {
-            $scope.message="Error";
-            $scope.messagecolor="alert alert-danger";
-        }
+angular.module('IoT').controller('loginController', function($scope) {
+    $scope.name = "John Doe";
+    var account = {
+      username : 'admin',
+      password : 'admin'
+    };
+    $scope.validate = function(usern, pwd){
+      if (usern === account.username && pwd === account.password  ) {
+        window.location.href="/home.html"
+        window.location.href="/index.html"
+        return true;
+      }
+      return false;
     }
-});
-	
-	
 
+});
