b47d3ee10a3756f3a7a5e147391dec633a903e2b
phamthem <38209756+phamthem@users.noreply.github.com>
2018-06-15 01:22:19 +0700

    Update login.js

--- webapp/login.js
+++ webapp/login.js
@@ -1,17 +1,40 @@
-var app = angular.module('loginModule', []);
-app.controller('loginController', function($scope) {
-    $scope.name = "John Doe";
-    var account = {
-      username : 'admin',
-      password : 'admin'
+(function(){
+ "use strict";
+ angular.module("User.DangNhap.Ctrl",[])
+  .controller("login", ["$scope", "$http", "MyAuth", "$location", function($scope, $http, MyAuth, $location) {
+   $scope.checkLogin = function() {
+    // call api for checking username/password
+    var user = {
+     username: $scope.username,
+     password: $scope.password
     };
-    $scope.validate = function(usern, pwd){
-      if (usern === account.username && pwd === account.password  ) {
-        window.location.href="/home.html"
-        window.location.href="/index.html"
-        return true;
+    $http.post("api/login", $.param(user)).
+     then(function(response) {
+      // check response
+      if(response.loginSuccess) {
+       // store user in localstore
+       // assump that user has role admin
+       user.permission = ["admin"];
+       MyAuth.storeUserInLocalStorage(user);
+       // redirect to /
+       $location.path("/");
       }
-      return false;
-    }
-
-});
+     }, function(error) {
+      // error
+      console.log(error);
+      console.log("Because we dont have real api for checking username, password");
+      console.log("So that, we hard code here!");
+      if($scope.username == "admin" && $scope.password == "admin") {
+       // store user in localstore
+       // assump that user has role admin
+       user.permission = ["admin"];
+       MyAuth.storeUserInLocalStorage(user);
+       // redirect to /
+       $location.path("/");
+      } else {
+       alert("Đăng nhập không thành công");
+      }
+     });
+   }
+  }]);
+})();
