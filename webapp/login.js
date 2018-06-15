a29a0b075fd5b3d5e6b3ba1cbb8f1032f4ddc27e
phamthem <38209756+phamthem@users.noreply.github.com>
2018-06-15 13:38:38 +0700

    Create login.js

---
+++ webapp/login.js
@@ -0,0 +1,23 @@
+	
+angular.module('IoT', [
+    'ngRoute',
+    'mobile-angular-ui',
+    'btford.socket-io'
+    ])
+.controller('Home', function($scope, mySocket) {
+	$scope.name = "John Doe";
+		var account = {
+		username : 'admin',
+		password : 'admin'
+		};
+	$scope.validate = function(usern, pwd){
+		if (usern === account.username && pwd === account.password  ) {
+			window.location.href="/home.html"
+			return true;
+			}
+		return false;
+		}
+});
+
+	
+	
