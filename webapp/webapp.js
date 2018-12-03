872ebe72da175857e58ed53c15674e29e73fcc2a
phamthem <38209756+phamthem@users.noreply.github.com>
2018-12-03 18:26:24 +0700

    Update webapp.js

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -50,6 +50,54 @@ angular.module('IoT', [
 	$scope.trangthai4relay = "Chưa kết nối";
 	
 	
+//khu do thi	
+	$(function () {
+    Highcharts.chart(\'chart1\', {
+        title: {
+            text: \'This is my first chart\',
+        },
+        xAxis: {
+            categories: [\'Jan\', \'Feb\', \'Mar\', \'Apr\', \'May\', \'Jun\',
+                \'Jul\', \'Aug\', \'Sep\', \'Oct\', \'Nov\', \'Dec\']
+        },
+        yAxis: {
+            title: {
+                text: \'Temperature (°C)\'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: \'#808080\'
+            }]
+        },
+        tooltip: {
+            valueSuffix: \'°C\'
+        },
+        legend: {
+            layout: \'vertical\',
+            align: \'right\',
+            verticalAlign: \'middle\',
+            borderWidth: 0
+        },
+        series: [{
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }]
+    });
+});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 //khu 2 -- gởi json về cho index.js đển chuyển về cho arduino
 	//Cách gửi tham số 1: dùng biến toàn cục! $scope.<tên biến> là biến toàn cục
 	$scope.dongrelay1 = function() {
