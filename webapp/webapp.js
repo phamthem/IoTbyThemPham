84296f2bf380d693e4bd81d6f8f6da313e07cf02
phamthem <38209756+phamthem@users.noreply.github.com>
2018-12-03 18:32:28 +0700

    Update webapp.js

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -51,41 +51,6 @@ angular.module('IoT', [
 	
 	
 //khu do thi	
-	$scope.(function () {
-    Highcharts.chart(\'chart1\', {
-        title: {
-            text: \'This is my first chart\',
-        },
-        xAxis: {
-            categories: [\'Jan\', \'Feb\', \'Mar\', \'Apr\', \'May\', \'Jun\',
-                \'Jul\', \'Aug\', \'Sep\', \'Oct\', \'Nov\', \'Dec\']
-        },
-        yAxis: {
-            title: {
-                text: \'Temperature (°C)\'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: \'#808080\'
-            }]
-        },
-        tooltip: {
-            valueSuffix: \'°C\'
-        },
-        legend: {
-            layout: \'vertical\',
-            align: \'right\',
-            verticalAlign: \'middle\',
-            borderWidth: 0
-        },
-        series: [{
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-        }]
-    });
-});
-	
-	
 	
 	
 	
