51dbe641415b3929bf29f91331dce912c7c34a59
phamthem <38209756+phamthem@users.noreply.github.com>
2018-12-05 12:31:32 +0700

    Update webapp.js

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -39,7 +39,7 @@ angular.module('IoT', [
 
 .controller('Home', function($scope, mySocket) {
 	
-	console.log("xxxxxxxxxxx ", account.username)
+	
 	
 ////Khu 1 -- Khu cài đặt tham số 
     	//cài đặt một số tham số test chơi
